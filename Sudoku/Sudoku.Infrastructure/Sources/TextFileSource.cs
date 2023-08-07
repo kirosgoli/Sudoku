83091905d3bf72cab2786220c769428065a00351
Karol Walczyna <kirosgoli@outlook.com>
2023-08-07 23:09:20 +0200

    Add simple models and base import operation

---
+++ Sudoku/Sudoku.Infrastructure/Sources/TextFileSource.cs
@@ -0,0 +1,25 @@
+﻿using Sudoku.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.Infrastructure.Sources
+{
+    internal class TextFileSource : FileSource
+    {
+
+        public TextFileSource() { }
+
+        public override Core.Models.Sudoku Convert(string path)
+        {
+            #region Init
+            ValidatePath(path);
+            #endregion
+
+            Core.Models.Sudoku result = new Core.Models.Sudoku();
+            return result;
+        }
+    }
+}
